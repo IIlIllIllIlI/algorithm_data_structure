304d792c68f581ad847281bcfe424dd66cf54dc3
IIlIllIllIlI <71163579+IIlIllIllIlI@users.noreply.github.com>
2024-12-20 19:50:20 +0800

    209

---
+++ src/leetcode/Q200-Q299/Q209.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+// #include "../../mystl/vector.h"
+// using namespace MySTL;
+
+#include <vector>
+using namespace std;
+
+namespace LeetCode {
+    class Q209 {
+    public:
+        int minSubArrayLen(int target, vector<int>& nums) {
+            Solution1 s;
+            return s.minSubArrayLen(target, nums);
+        }
+
+    private:
+        class Solution1 {
+            // Sliding Window O(n)
+        public:
+            int minSubArrayLen(int target, vector<int>& nums) {
+                int numsSize = nums.size();
+                int l = 0;
+                int r = 0;
+                int minSoFar = numsSize + 1;
+                int sum = 0;
+
+                while (r < numsSize) {
+                    sum += nums[r];
+                    while (sum >= target) {
+                        if (r - l + 1 < minSoFar) {
+                            minSoFar = r - l + 1;
+                        }
+                        sum -= nums[l];
+                        l++;
+                    }
+                    r++;
+                }
+
+                if (minSoFar > numsSize) {
+                    return 0;
+                } else {
+                    return minSoFar;
+                }
+            }
+        };
+    };
+}
---
+++ test/leetcode_test/current/Q209.cpp
@@ -0,0 +1,25 @@
+﻿#include "Q200-Q299/Q209.h"
+#include <gtest/gtest.h>
+
+// Constraints:
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums is sorted in non-decreasing order.
+
+namespace {
+    LeetCode::Q209 s;
+    TEST(Q209, Example1) {
+        vector<int> nums{2, 3, 1, 2, 4, 3};
+        EXPECT_EQ(s.minSubArrayLen(7, nums), 2);
+    }
+
+    TEST(Q209, Example2) {
+        vector<int> nums{1, 4, 4};
+        EXPECT_EQ(s.minSubArrayLen(4, nums), 1);
+    }
+
+    TEST(Q209, Example3) {
+        vector<int> nums{1, 1, 1, 1, 1, 1, 1, 1};
+        EXPECT_EQ(s.minSubArrayLen(11, nums), 0);
+    }
+}
