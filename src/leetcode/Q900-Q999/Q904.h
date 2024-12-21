d328603455e9cc0e6353bba422083dfe1b4c1598
IIlIllIllIlI <71163579+IIlIllIllIlI@users.noreply.github.com>
2024-12-21 12:59:47 +0800

    Q904 未完待续

---
+++ src/leetcode/Q900-Q999/Q904.h
@@ -0,0 +1,25 @@
+﻿// 904. Fruit Into Baskets
+
+#pragma once
+#include "../../mystl/vector.h"
+using namespace MySTL;
+
+namespace LeetCode {
+    class Q904 {
+    public:
+        int totalFruit(vector<int>& fruits) {
+            Solution1 s;
+            return s.totalFruit(fruits);
+        }
+
+        // 1,2,3,2,2
+    private:
+        class Solution1 {
+        public:
+            int totalFruit(vector<int>& fruits) {
+
+                return 0;
+            }
+        };
+    };
+}
---
+++ test/leetcode_test/current/Q904.cpp
@@ -0,0 +1,21 @@
+﻿#include "Q900-Q999/Q904.h"
+#include <algorithm>
+#include <gtest/gtest.h>
+
+namespace {
+    LeetCode::Q904 s;
+    TEST(Q904, Example1) {
+        vector<int> fruits{1, 2, 1};
+        EXPECT_EQ(s.totalFruit(fruits), 3);
+    }
+
+    TEST(Q904, Example2) {
+        vector<int> fruits{0, 1, 2, 2};
+        EXPECT_EQ(s.totalFruit(fruits), 3);
+    }
+
+    TEST(Q904, Example3) {
+        vector<int> fruits{1, 2, 3, 2, 2};
+        EXPECT_EQ(s.totalFruit(fruits), 4); // 2, 3, 2, 2
+    }
+}
